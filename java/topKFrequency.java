fb19a6add81cb91827bb7fa05bf454ed26be2d88
John Mansfield <jmansfield6@gatech.edu>
2019-04-07 08:42:16 -0400

    topKFrequency.java

--- java/topKFrequency.java
+++ java/topKFrequency.java
@@ -3,14 +3,48 @@ import java.util.*;
 public class topKFrequency {
 
     public static List<Integer> topKFrequent(int[] nums, int k){
-        HashMap hashMap=new HashMap();
 
+        List<Integer> returnList = new LinkedList<>();
+
+        //hash nums
+        HashMap<Integer, Integer> hashMap=new HashMap();
         for (int i=0; i<nums.length; i++){
             int value = hashMap.get(nums[i])==null? 1 : (int) hashMap.get(nums[i]) + 1;
             hashMap.put(nums[i], value);
         }
-        return getTopK(hashMap, k);
+
+        //using second hashmap (derp)
+        HashMap<Integer, List<Integer>> secondMap=new HashMap();
+        LinkedList<Integer> tempList;
+        for (Map.Entry<Integer, Integer> entry: hashMap.entrySet()){
+            if (secondMap.containsKey(entry.getValue())){
+                secondMap.get(entry.getValue()).add(entry.getKey());
+            }
+            else {
+                tempList=new LinkedList<>();
+                tempList.add(entry.getKey());
+                secondMap.put(entry.getValue(), tempList);
+            }
+        }
+
+        //add top k to returnList
+        for (int i=nums.length; i>0; i--) {
+            if (k == 0) {
+                return returnList;
+            }
+            if (secondMap.containsKey(i)) {
+                for (int num : secondMap.get(i)) {
+                    k--;
+                    returnList.add(num);
+                }
+            }
+        }
+
+        return returnList;
     }
+
+    //this part is a bit kludgy and slow, but can also be used instead
+    //of second hashmap
 
     //Sort list of lists from hashmap and append top k elts to new list
     private static List<Integer> getTopK(HashMap hashMap, int k){
