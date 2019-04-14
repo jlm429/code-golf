5a0cbbbcfe5f8e5f8ae37dfa45b7e82fb4177f8f
John Mansfield <jmansfield6@gatech.edu>
2019-04-14 06:56:33 -0400

    KthSmallestBST.java

---
+++ java/KthSmallestBST.java
@@ -0,0 +1,30 @@
+public class KthSmallestBST {
+
+    private int count=0;
+    private int k;
+    private Integer elt=null;
+
+
+    public int kthSmallest(TreeNode root, int k) {
+
+        inOrder(root);
+        return this.elt;
+    }
+
+
+
+    private void inOrder(TreeNode node){
+        if (node.left!=null && this.elt==null){
+            inOrder(node.left);
+        }
+        if (this.count<this.k){
+            this.count++;
+            if (this.count==this.k){
+                this.elt=node.val;
+            }
+        }
+        if (node.right!=null && this.elt==null){
+            inOrder(node.right);
+        }
+    }
+}
