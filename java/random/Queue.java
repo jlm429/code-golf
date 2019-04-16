d08c1b049a05ce4664a0e1fdf1fc34790b793791
John Mansfield <jmansfield6@gatech.edu>
2019-04-16 18:56:07 -0400

    Queue.java using generics

---
+++ java/random/Queue.java
@@ -0,0 +1,41 @@
+public class Queue<T> {
+
+    private ListNode3 head;
+    private ListNode3 tail;
+
+    public Queue(){
+
+    }
+
+    public void enQueue(T obj){
+        if (head==null){
+            head=new ListNode3(obj);
+            tail=head;
+        }
+        else {
+            ListNode3 node=new ListNode3(obj);
+            tail.next=node;
+            tail=node;
+        }
+    }
+
+    public T deQueue(){
+        if (head==null) throw new NullPointerException();
+        else {
+            T obj=(T) head.val;
+            head=head.next;
+            return obj;
+        }
+    }
+
+    public T peekFirst(){
+        if (head==null) throw new NullPointerException();
+        return (T) head.val;
+    }
+
+    public T peekLast(){
+        if (tail==null) throw new NullPointerException();
+        return (T) tail.val;
+    }
+
+}
